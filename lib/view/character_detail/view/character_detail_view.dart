364e6ccb2757402a452acf5092fb703d7c4ddc42
hasantunahan <hasantunahan12345@gmail.com>
2022-05-28 20:25:16 +0300

    character detail

--- lib/view/character_detail/view/character_detail_view.dart
+++ lib/view/character_detail/view/character_detail_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:marvel_api/_product/config/navigation/custom_navigation_route.dart';
+import 'package:marvel_api/_product/constant/padding.dart';
 import 'package:marvel_api/_product/widget/marve_base_widget/marvel_base_widget.dart';
 import 'package:marvel_api/core/base/view/base_view.dart';
 import 'package:marvel_api/core/config/navigation/arguments.dart';
@@ -46,38 +47,67 @@ class _CharacterDetailViewState extends State<CharacterDetailView> {
         return MarvelBaseWidget(
           hideAppBar: true,
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Stack(
-                children: [
-                  Image.network(
-                    "${_args.thumbnail!.path}/standard_amazing.jpg",
-                    height: context.width,
-                    fit: BoxFit.cover,
-                    width: context.width,
+              _renderTopImage(context),
+              Padding(
+                padding: AppPadding.instance.allPaddingMedium,
+                child: Text(
+                  _args.name!,
+                  style: context.theme.textTheme.headline5
+                      ?.copyWith(fontWeight: FontWeight.w600, color: Colors.black, letterSpacing: 0.24),
+                ),
+              ),
+              Padding(
+                padding: AppPadding.instance.symmetricPaddingHorizontalMedium,
+                child: Text(
+                  _args.description!,
+                  style: const TextStyle(
+                    fontSize: 13,
+                    letterSpacing: 0.24,
                   ),
-                  Positioned(
-                    top: MediaQuery.of(context).viewInsets.top + 44,
-                    left: 12.0,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.black26,
-                      child: IconButton(
-                        onPressed: () {
-                          NavigationService.instance.back();
-                        },
-                        splashColor: context.theme.colorScheme.primary,
-                        icon: const Icon(
-                          Icons.arrow_back,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ],
           ),
         );
       },
+    );
+  }
+
+  Widget _renderTopImage(BuildContext context) {
+    return Stack(
+      children: [
+        Image.network(
+          "${_args.thumbnail!.path}/landscape_incredible.jpg",
+          height: context.width*.5625,
+          fit: BoxFit.contain,
+          width: context.width,
+          errorBuilder: (context, error, stackTrace) {
+            return Container(
+              color: Colors.grey.withOpacity(0.2),
+              height: context.width,
+            );
+          },
+        ),
+        Positioned(
+          top: MediaQuery.of(context).viewInsets.top + 44,
+          left: 12.0,
+          child: CircleAvatar(
+            backgroundColor: Colors.black26,
+            child: IconButton(
+              onPressed: () {
+                NavigationService.instance.back();
+              },
+              splashColor: context.theme.colorScheme.primary,
+              icon: const Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
